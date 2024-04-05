f57c3a7b01bd1f75885f7867418bd182c05d36e3
Santiago Giaccobasso <santigiacco@192.168.0.103>
2024-04-04 22:17:31 -0300

    add basic functionality and tailwind config

---
+++ axelar.ts
@@ -0,0 +1,38 @@
+import {
+  AxelarAssetTransfer,
+  AxelarQueryAPI,
+} from "@axelar-network/axelarjs-sdk";
+
+export async function getEstimatedFee(fromChain, toChain, asset, amount, env) {
+  const axelarQuery = new AxelarQueryAPI({
+    environment: env,
+  });
+
+  const fee = await axelarQuery.getTransferFee(
+    fromChain,
+    toChain,
+    asset,
+    amount
+  );
+  return fee;
+  // returns  { fee: { denom: 'uausdc', amount: '150000' } }
+}
+
+export async function transfer(
+  asset,
+  fromChain,
+  toChain,
+  destinationAddress,
+  env
+) {
+  console.log("tochain", toChain);
+  const axelarAssetTransfer = new AxelarAssetTransfer({
+    environment: env,
+  });
+  return axelarAssetTransfer.getDepositAddress({
+    fromChain,
+    toChain,
+    destinationAddress,
+    asset,
+  });
+}
