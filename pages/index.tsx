4266cf54f4d63f60420c55121a2ff2054fad280a
Santiago Giaccobasso <santigiacco@192.168.0.103>
2024-04-10 23:44:16 -0300

    add icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,6 +95,7 @@ const Home: NextPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <Head>
         <title>Token Bridge</title>
+        <link href="/favicon.ico" rel="icon" />
       </Head>
 
       <main className="p-6 bg-white rounded-lg shadow-md w-full max-w-lg">
